bfa89b1d513a28b898c4f335b56fdf04a466844a
Sri Hasini Parsa <152248394+hasini2709@users.noreply.github.com>
2024-10-03 21:08:47 +0530

    Update Interview.js

--- Interview.js
+++ Interview.js
@@ -5,32 +5,32 @@ document.addEventListener('DOMContentLoaded', function() {
     const modals = document.querySelectorAll('.modal');
     const closeButtons = document.querySelectorAll('.close-btn');
 
-    // Open modal and prevent background scrolling
+  
     toggleButtons.forEach(function(button) {
         button.addEventListener('click', function() {
             const targetModal = button.getAttribute('data-target');
             const modal = document.getElementById(targetModal);
             modal.style.display = 'flex';
-            document.body.classList.add('body-no-scroll');  // Disable body scroll
+            document.body.classList.add('body-no-scroll');  
         });
     });
    
-    // Close modal and re-enable background scrolling
+   
     closeButtons.forEach(function(button) {
         button.addEventListener('click', function() {
             const targetModal = button.getAttribute('data-close');
             const modal = document.getElementById(targetModal);
             modal.style.display = 'none';
-            document.body.classList.remove('body-no-scroll');  // Re-enable body scroll
+            document.body.classList.remove('body-no-scroll');  
         });
     });
 
-    // Close modal if clicked outside the modal content
+  
     window.onclick = function(event) {
         modals.forEach(function(modal) {
             if (event.target == modal) {
                 modal.style.display = 'none';
-                document.body.classList.remove('body-no-scroll');  // Re-enable body scroll
+                document.body.classList.remove('body-no-scroll'); 
             }
         });
     };
